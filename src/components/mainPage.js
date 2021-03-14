45c92ece469a5d2ec1ad368f5f9daf3ea8c3b081
tobi-oye <tobiloba5000@gmail.com>
2021-03-14 13:09:53 +0100

    [feat]- main page component

---
+++ src/components/mainPage.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {Object.values(this.props.bookShelves).map((book) => {
+              return (
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">{book.shelveName}</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {book.books.map((img) => {
+                        return (
+                          <li>
+                            <div className="book">
+                              <div className="book-top">
+                                <div
+                                  className="book-cover"
+                                  style={{
+                                    width: 128,
+                                    height: 193,
+                                    backgroundImage: `url(${
+                                      img.imageLinks.smallThumbnail
+                                    })`,
+                                  }}
+                                />
+
+                                <div className="book-shelf-changer">
+                                  <select
+                                    onChange={(e) => {
+                                      return this.props.moveBook(book, e, img);
+                                    }}
+                                    defaultValue={book.id}
+                                  >
+                                    <option value="move" disabled>
+                                      Move to...
+                                    </option>
+                                    {Object.values(this.props.options).map(
+                                      (elem) => {
+                                        return (
+                                          <option value={elem.id} key={elem.id}>
+                                            {" "}
+                                            {elem.label}{" "}
+                                          </option>
+                                        );
+                                      }
+                                    )}
+                                  </select>
+                                </div>
+                              </div>
+                              <div className="book-title">{img.title}</div>
+                              <div className="book-authors">
+                                {img.authors[0]}
+                              </div>
+                            </div>
+                          </li>
+                        );
+                      })}
+                    </ol>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/searchPage">
+            <button />
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
